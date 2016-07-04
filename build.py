8502e255585222e3034a3a5a0dcb7df938fa92dc
Tim Lebedkov <tim.lebedkov@gmail.com>
2016-07-04 13:07:30 +0200

    enable GCC builds on Windows

--- build.py
+++ build.py
@@ -1,7 +1,14 @@
 from conan.packager import ConanMultiPackager
-
+import platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="zlib:shared", pure_c=True)
+
+    # the following code enables the GCC builds on Windows and
+    # should be moved later into conan-io/conan-package-tools
+    if platform.system() == "Windows":
+        builder._add_linux_gcc_builds(shared_option_name="zlib:shared", 
+            pure_c=True)
+        
     builder.run()
